373e24659d6ab7b6f21ce4a7302cc45d0e8f3c3c
Jessica Mcdonald <jmcdon53@uncc.edu>
2025-03-31 08:48:12 -0400

    profile page phase 1

--- base/models.py
+++ base/models.py
@@ -35,3 +35,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.body[:50]
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    enrolled_courses = models.ManyToManyField(Group, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import search_results, login_page
+from .views import search_results, login_page, profile_page
 
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('groups/<str:pk>/', views.group, name="groups"),
     path('search/', search_results, name='search_results'),
-    path('login', login_page, name='login'),
+    path('login/', login_page, name='login'),
+    path('profile', profile_page, name='profile'),
 ]
